01909869ea21a19a2be62694ad69e85f055a9519
GaleanoSantiago <104083617+GaleanoSantiago@users.noreply.github.com>
2024-01-22 21:12:04 -0300

    mejor separacion entre elementos del rightSideBar

--- resources/views/frontend/pages/disenio2.blade.php
+++ resources/views/frontend/pages/disenio2.blade.php
@@ -397,22 +397,17 @@
                         <div class="right-functions">
                             <!--------------------------- Para descargar como pdf --------------------------->
                             
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col text-center">
-                                        <h5>Descargar en PDF</h5>
-                                        <button class="button" type="button" id="download-pdf" >
-                                            <span class="button__text">Descargar PDF</span>
-                                            <span class="button__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" id="bdd05811-e15d-428c-bb53-8661459f9307" data-name="Layer 2" class="svg"><path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path><path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path><path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path></svg></span>
-                                        </button>
-                                    </div>
-                                    <div class="col text-center">
-                                        <h5>Ver mi vaso en 3D</h5>
-                                        <button class="btn3d" id="ver3DBtn"> Ver 3D
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
+                            <h5>Descargar en PDF</h5>
+                            <button class="button" type="button" id="download-pdf" >
+                                <span class="button__text">Descargar PDF</span>
+                                <span class="button__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" id="bdd05811-e15d-428c-bb53-8661459f9307" data-name="Layer 2" class="svg"><path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path><path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path><path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path></svg></span>
+                            </button>
+                                        
+                        </div>
+                        <div class="right-functions vaso3D-section">
+                            <!--------------------------- Para Visualizar Modelo 3D --------------------------->
+                            <h5>Visualizar Vaso en 3D</h5>
+                            <button class="btn3d" id="ver3DBtn">Ver 3D</button>
                         </div>
                     </div>
                     <!--------------------------- Elemento Canva --------------------------->
