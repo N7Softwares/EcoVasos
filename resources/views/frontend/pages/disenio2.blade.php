944eee0bc12efc5e1ff6bb0a192cd6daba57f9a4
Miguel Gomez <68757234+Miguek22@users.noreply.github.com>
2024-02-06 10:32:55 -0300

    cambios

--- resources/views/frontend/pages/disenio2.blade.php
+++ resources/views/frontend/pages/disenio2.blade.php
@@ -556,10 +556,7 @@
                         </div>
                     </div>
                     <div id="svg-container-brand" class="d-none">
-                        <?xml version="1.0" standalone="no"?>
-                        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN"
-                        "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
-                        <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+                        <svg xmlns="http://www.w3.org/2000/svg"
                         width="975.000000pt" height="922.000000pt" viewBox="0 0 975.000000 922.000000"
                         preserveAspectRatio="xMidYMid meet">
                         <metadata>
