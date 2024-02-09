208fb0fd4f38f42826c7f9981de49f85a817d0c4
Miguel Gomez <68757234+Miguek22@users.noreply.github.com>
2024-02-09 19:30:50 -0300

    mimificacion

--- public/frontend/js/passwordRecovery.js
+++ public/frontend/js/passwordRecovery.js
@@ -1,15 +1,1 @@
-const loginBtn = document.querySelector('.login-btn');
-const input = document.querySelector('.prEmail');
-
-loginBtn.addEventListener('click', function(event){
-    event.preventDefault();
-        
-            console.log('shit')
-        
-            document.querySelector('.psrInfo').classList.add('displayNone');
-            document.querySelector('#passwordRecovery').classList.add('displayNone');
-        
-            document.querySelector('.psrMsg').classList.remove('displayNone');
-    
-
-});
+const loginBtn=document.querySelector(".login-btn"),input=document.querySelector(".prEmail");loginBtn.addEventListener("click",(function(e){e.preventDefault(),console.log("shit"),document.querySelector(".psrInfo").classList.add("displayNone"),document.querySelector("#passwordRecovery").classList.add("displayNone"),document.querySelector(".psrMsg").classList.remove("displayNone")}));
